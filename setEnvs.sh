cfd0015fa28a4af55e35fecefe2d00f005721753
Carlo Feliciano Aureus <carlo_aureus@intuit.com>
2025-07-12 21:45:23 -0700

    Initial commit

---
+++ setEnvs.sh
@@ -0,0 +1,3 @@
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/amazon-corretto-21.jdk/Contents/Home
+export MVN_HOME=~/Applications/apache-maven-3.9.10
+export PATH=$MAVEN_HOME/bin:$PATH
---
+++ src/main/java/com/intuit/graphql/examples/petstoredemo/PetstoredemoApplication.java
@@ -0,0 +1,13 @@
+package com.intuit.graphql.examples.petstoredemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetstoredemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetstoredemoApplication.class, args);
+	}
+
+}
